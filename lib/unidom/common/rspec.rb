8eac4ccdaf73814a6eb8138fd6906f5ab77d4f76
Topbit Du <topbit.du@gmail.com>
2016-12-27 13:44:45 +0800

    1, Improve the RSpec module to require the Has Many shared examples.

--- lib/unidom/common/rspec.rb
+++ lib/unidom/common/rspec.rb
@@ -4,10 +4,11 @@ require 'unidom/common/rspec/scope_shared_examples'
 require 'unidom/common/rspec/validates_shared_examples'
 require 'unidom/common/rspec/model_extension_shared_examples'
 
+require 'unidom/common/rspec/has_many_shared_examples'
+
 module Unidom
   module Common
     module RSpec
-      # Your code goes here...
     end
   end
 end
