89854f692916c808f1a648bad8970d7aeec72a29
Topbit Du <topbit.du@gmail.com>
2016-12-27 09:39:02 +0800

    1, Improve the RSpec module to require the Model Extension shared examples.

--- lib/unidom/common/rspec.rb
+++ lib/unidom/common/rspec.rb
@@ -2,6 +2,7 @@ require "unidom/common/rspec/version"
 
 require 'unidom/common/rspec/scope_shared_examples'
 require 'unidom/common/rspec/validates_shared_examples'
+require 'unidom/common/rspec/model_extension_shared_examples'
 
 module Unidom
   module Common
