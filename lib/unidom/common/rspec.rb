06c4b178d0b61fe450ccd28c08aad9cf4b17caa4
Topbit Du <topbit.du@gmail.com>
2016-12-31 12:35:57 +0800

    1, Improve the RSpec module to require the Belongs To shared examples.

--- lib/unidom/common/rspec.rb
+++ lib/unidom/common/rspec.rb
@@ -5,6 +5,7 @@ require 'unidom/common/rspec/validates_shared_examples'
 require 'unidom/common/rspec/model_extension_shared_examples'
 
 require 'unidom/common/rspec/has_many_shared_examples'
+require 'unidom/common/rspec/belongs_to_shared_examples'
 
 module Unidom
   module Common
