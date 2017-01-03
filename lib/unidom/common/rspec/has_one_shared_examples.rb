1e354a46f0d1aca1dfd4ebfd1ff2cd9baee53296
Topbit Du <topbit.du@gmail.com>
2017-01-03 14:58:27 +0800

    1, Improve the Has One shared examples for the #build_association method, the #create_association method, & the #create_association! method.

--- lib/unidom/common/rspec/has_one_shared_examples.rb
+++ lib/unidom/common/rspec/has_one_shared_examples.rb
@@ -10,115 +10,61 @@ shared_examples 'has_one' do |model_attributes, association_name, association_cl
     gained_methods << [ :"#{association_name}=", :"build_#{association_name}", :"create_#{association_name}", :"create_#{association_name}!" ] if association_writable
     gained_methods.flatten!
 
-    #subject :model_instance       do described_class.new   model_attributes                         end
-    #let     :association_instance do association_class.new association_attributes                   end
-    #let     :reflection           do model_instance.association(association_name.to_sym).reflection end
-    #let     :association_writable do reflection.class==ActiveRecord::Reflection::HasOneReflection   end
+    subject do described_class.new model_attributes end
 
-    before :each do
-      subject do
-        described_class.new model_attributes
+    gained_methods.each do |method|
+      it do is_expected.to respond_to(method.to_sym) end
+    end
+
+    describe "#{association_name}=" do
+      before :each do
+        subject.send "#{association_name}=", nil
+        subject.send "#{association_name}=", association_instance
+      end
+      it 'should return the assigned association' do
+        expect(subject.send association_name.to_sym).to eq(association_instance)
       end
     end
 
-    after :each do
-      subject do
-        nil
-      end
-    end
-
-    #describe do
-      gained_methods.each do |method|
-        it do is_expected.to respond_to(method.to_sym) end
-      end
-    #end
-
-    it 'assigns association successfully' do
-      model_instance.send "#{association_name}=", association_instance
-      expect(model_instance.send association_name.to_sym).to eq(association_instance)
-    end
-=begin
     if association_writable
-      context do
-      it 'builds an association instance' do
-        item_1 = model_instance.send "build_#{association_name}"
-        expect(item_1).to     be_an_instance_of(association_class)
-        expect(item_1).to     be_new_record
-        item_2 = model_instance.send "build_#{association_name}"
-        expect(item_2).to     be_an_instance_of(association_class)
-        expect(item_2).to     be_new_record
-        expect(item_1).to_not be(item_2)
+
+      context "#build_#{association_name}" do
+        let :association_instance_1                 do subject.send                          "build_#{association_name}" end
+        let :association_instance_2                 do subject.send                          "build_#{association_name}" end
+        it  "should be a #{association_class.name}" do expect(association_instance_1).to     be_a(association_class)     end
+        it  'should not be a new record'            do expect(association_instance_1).to     be_new_record               end
+        it  "should be a #{association_class.name}" do expect(association_instance_2).to     be_a(association_class)     end
+        it  'should not be a new record'            do expect(association_instance_2).to     be_new_record               end
+        it  'should not be identical'               do expect(association_instance_1).to_not be(association_instance_2)  end
       end
-      end
-      context do
-        before :each do
-          puts '---- before each...'
-          subject do described_class.new   model_attributes                         end
-            puts "----!!!! subject = #{subject.inspect}"
-            puts "subject.clear_text = #{subject.clear_text}"
-            puts "model_attributes = #{model_attributes.inspect}"
+
+      [ "create_#{association_name}", "create_#{association_name}!" ].each do |method_name|
+        context "##{method_name}" do
+          subject do described_class.create! model_attributes end
+          let :created_association_instance do subject.send method_name, association_attributes end
+          it "should be a #{association_class.name}" do expect(created_association_instance).to     be_an_instance_of(association_class) end
+          it 'should not be a new record'            do expect(created_association_instance).to_not be_new_record                        end
         end
-      it 'creates an association instance' do
-subject do described_class.new   model_attributes                         end
-subject.valid?
-        puts "---- errors = #{subject.errors.inspect}"
-puts "---- **** subject = #{subject.inspect}"
-
-        expect(subject.save).to be_truthy
-
-        created_association_instance = subject.send "create_#{association_name}", association_attributes
-        expect(created_association_instance).to     be_an_instance_of(association_class)
-        expect(created_association_instance).to_not be_new_record
       end
-      end
-    end
-=end
+
+      # Pending
 =begin
-    [ association_name, "#{association_name}=".to_sym, "build_#{association_name}".to_sym, "create_#{association_name}".to_sym ].each do |method|
-      association_writable = #model_instance.reflections[association_name].class==::ActiveRecord::Reflection::AssociationReflection
-        #model_instance._reflections[association_name].class==ActiveRecord::Reflection::AssociationReflection
-        model_instance.association(association_name)._reflections.class==ActiveRecord::Reflection::AssociationReflection
-      describe ".#{method}" do
-
-        if association_writable
-          it 'is responded to' do expect(model_instance).to respond_to(method.to_sym) end
-        else
-          unless [ "build_#{association_name}".to_sym, "create_#{association_name}".to_sym ].include? method
-            it 'is responded to' do expect(model_instance).to respond_to(method.to_sym) end
-          end
-        end
-
-        case method
-          when "build_#{association_name}".to_sym
-            if association_writable
-              it 'builds an association instance' do
-                item_1 = model_instance.send method
-                expect(item_1).to     be_an_instance_of(association_class)
-                expect(item_1).to     be_new_record
-                item_2 = model_instance.send method
-                expect(item_2).to     be_an_instance_of(association_class)
-                expect(item_2).to     be_new_record
-                expect(item_1).to_not be(item_2)
-              end
-            end
-          when "create_#{association_name}".to_sym
-            if association_writable
-              it 'creates an association instance' do
-                pending 'Can not create association instance.'
-                created_association_instance = model_instance.send method, association_attributes
-                expect(created_association_instance).to     be_an_instance_of(association_class)
-                expect(created_association_instance).to_not be_new_record
-              end
-            end
-          when "#{association_name}=".to_sym
-            it 'assigns association successfully' do
-              model_instance.send method, association_instance
-              expect(model_instance.send association_name.to_sym).to eq(association_instance)
-            end
+      context "#create_#{association_name}" do
+        subject do described_class.create! model_attributes end
+        it 'should return false with {}' do
+          expect { subject.send "create_#{association_name}", {} }.to be_falsey
         end
       end
-    end
+
+      context "#create_#{association_name}!" do
+        subject do described_class.create! model_attributes end
+        it 'should raise error with {}' do
+          expect { subject.send "create_#{association_name}!", {} }.to raise_error(ActiveRecord::RecordInvalid)
+        end
+      end
 =end
+
+    end
 
   end
 
