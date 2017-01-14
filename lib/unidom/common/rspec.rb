ef31779de458616f43415ee6ba398a4454f5407e
Topbit Du <topbit.du@gmail.com>
2017-01-14 21:42:09 +0800

    1, Improve the RSpec module to require the Each Validator shared examples.

--- lib/unidom/common/rspec.rb
+++ lib/unidom/common/rspec.rb
@@ -1,5 +1,7 @@
 require "unidom/common/rspec/version"
 
+require 'unidom/common/rspec/each_validator_shared_examples'
+
 require 'unidom/common/rspec/scope_shared_examples'
 require 'unidom/common/rspec/validates_shared_examples'
 
