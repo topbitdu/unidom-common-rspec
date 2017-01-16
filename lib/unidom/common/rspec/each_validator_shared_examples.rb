5c834f0fc415a73bf4a1d9f524de871965a7539c
Topbit Du <topbit.du@gmail.com>
2017-01-17 07:28:21 +0800

    1, Each Validator shared examples.

--- lib/unidom/common/rspec/each_validator_shared_examples.rb
+++ lib/unidom/common/rspec/each_validator_shared_examples.rb
@@ -1,31 +1,32 @@
-shared_examples 'ActiveModel::EachValidator' do |validator, valid_values, invalid_values|
+shared_examples 'ActiveModel::EachValidator' do |valid_values, invalid_values|
 
-  class EachValidatorMockModel
+  mock_model_class_name = "#{described_class.name}MockModel"
+  mock_model_namespace  = mock_model_class_name.deconstantize.constantize
+  mock_model_name       = mock_model_class_name.demodulize
+  validator_name        = described_class.to_s.underscore.chomp('_validator').to_sym
+  mock_model_namespace.const_set mock_model_name, Class.new
+  class_instance        = mock_model_namespace.const_get mock_model_name
 
-    include ActiveModel::Model
-
-    attr_accessor :attribute
-
-    #validates :attribute, :allow_blank => true, validator.to_sym => true
-
+  class_instance.class_eval do
+    include       ActiveModel::Model
+    attr_accessor :mock_attribute
+    validates     :mock_attribute, :allow_blank => true, validator_name => true
   end
 
-  EachValidatorMockModel.validates :attribute, :allow_blank => true, validator.to_sym => true
-
-  subject(:model) do EachValidatorMockModel.new end
+  subject(:model) do class_instance.new end
 
   describe '#validate_each' do
 
     valid_values.each do |value|
       it "should validate valid #{value}" do
-        model.attribute = value
+        model.mock_attribute = value
         expect(model.valid?).to be_truthy
       end
     end
 
     invalid_values.each do |value|
       it "should validate invalid #{value}" do
-        model.attribute = value
+        model.mock_attribute = value
         expect(model.valid?).to be_falsey
       end
     end
