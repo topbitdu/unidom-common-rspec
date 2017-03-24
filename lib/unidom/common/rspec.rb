7b66590e9fe31f45def58d085f25f8b7fa8b82e8
Topbit Du <topbit.du@gmail.com>
2017-03-24 20:07:16 +0800

    1, Improve the RSpec shared examples manifest to add the Assert Present shared examples.

--- lib/unidom/common/rspec.rb
+++ lib/unidom/common/rspec.rb
@@ -1,5 +1,7 @@
 require "unidom/common/rspec/version"
 
+require 'unidom/common/rspec/assert_present_shared_examples'
+
 require 'unidom/common/rspec/each_validator_shared_examples'
 
 require 'unidom/common/rspec/scope_shared_examples'
