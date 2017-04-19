667aa120884336d7e34069090b6a15c7e98c2812
Topbit Du <topbit.du@gmail.com>
2017-04-19 15:55:06 +0800

    1, Improve the RSpec shared examples manifest to require the Monomorphic Scope shared examples, the Polymorphic Scope shared examples, the Validates Numericality shared examples, & the Validates Text shared examples.

--- lib/unidom/common/rspec.rb
+++ lib/unidom/common/rspec.rb
@@ -5,7 +5,12 @@ require 'unidom/common/rspec/assert_present_shared_examples'
 require 'unidom/common/rspec/each_validator_shared_examples'
 
 require 'unidom/common/rspec/scope_shared_examples'
+require 'unidom/common/rspec/monomorphic_scope_shared_examples'
+require 'unidom/common/rspec/polymorphic_scope_shared_examples'
+
 require 'unidom/common/rspec/validates_shared_examples'
+require 'unidom/common/rspec/validates_numericality_shared_examples'
+require 'unidom/common/rspec/validates_text_shared_examples'
 
 require 'unidom/common/rspec/belongs_to_shared_examples'
 require 'unidom/common/rspec/has_many_shared_examples'
